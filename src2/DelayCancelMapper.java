979415771b6992185c795cc992cd889db8d91534
Aditya KS <kammardisathyanara.a@husky.neu.edu>
2017-10-20 02:57:12 -0400

    Adding

--- src2/DelayCancelMapper.java
+++ src2/DelayCancelMapper.java
@@ -21,19 +21,21 @@ public class DelayCancelMapper extends Mapper<Object, Text, Text, Text> {
         InterruptedException {
 //        String[] record = value.toString().split(CSV_SEP);
         CSVRecord record = new CSVRecord(value.toString());
-        if (record.get(0).compareTo(inputYear) < 0) {
-           StringBuilder sb = new StringBuilder();
-           sb.append(record.get(2));
-           sb.append(CSV_SEP);
-           sb.append(record.get(6).replaceAll("\\s+", ""));
-           sb.append(CSV_SEP);
-           sb.append(record.get(14));
-           sb.append(CSV_SEP);
-           sb.append(record.get(23));
-           outputKey.set(sb.toString());
-           String output = (record.get(47).equals("1"))? record.get(43) + CSV_SEP + "1": record.get(43) + CSV_SEP + "0";
-           outputValue.set(output);
-           context.write(outputKey,outputValue);
+        if(CommonUtil.cleanData(record)){
+            if (record.get(0).compareTo(inputYear) < 0) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(record.get(2));
+                sb.append(CSV_SEP);
+                sb.append(record.get(6).replaceAll("\\s+", ""));
+                sb.append(CSV_SEP);
+                sb.append(record.get(14));
+                sb.append(CSV_SEP);
+                sb.append(record.get(23));
+                outputKey.set(sb.toString());
+                String output = (record.get(47).equals("1"))? record.get(43) + CSV_SEP + "1": record.get(43) + CSV_SEP + "0";
+                outputValue.set(output);
+                context.write(outputKey,outputValue);
+            }
         }
     }
 }
